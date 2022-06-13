c29a5082cf0909a195935c32ea87000016177b56
Matthieu Mota <matthieumota@gmail.com>
2022-06-13 18:43:28 +0200

    Beers from api

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import Header from './components/Header';
 import Results from './components/Results';
 import Search from './components/Search';
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      beers: [
-        { name: 'Bière 1', image: 'logo192.png' },
-        { name: 'Bière 2', image: 'logo192.png' },
-        { name: 'Bière 3', image: 'logo192.png' },
-        { name: 'Bière 4', image: 'logo192.png' },
-      ]
+      beers: [],
     };
+  }
+
+  componentDidMount() {
+    this.loadBeers();
+  }
+
+  loadBeers() {
+    axios.get('https://api.punkapi.com/v2/beers').then(response => this.setState({ beers: response.data }));
   }
 
   render() {
--- src/components/Results.js
+++ src/components/Results.js
@@ -4,7 +4,7 @@ function Results(props) {
             {props.beers.map((beer) =>
                 <div className="beer" key={beer.id}>
                     <h2>{beer.name}</h2>
-                    <img src={beer.image} alt={beer.name} />
+                    <img src={beer.image_url} alt={beer.name} />
                 </div>
             )}
         </div>
